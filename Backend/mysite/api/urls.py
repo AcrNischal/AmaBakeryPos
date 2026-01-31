3d4b1f3680982ea7842e1e52cc72800c71463510
Acrsahil <acharyasahil2003@gmail.com>
2026-01-31 15:35:34 +0545

    fix:Urls

--- Backend/mysite/api/urls.py
+++ Backend/mysite/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("users/", views.UserView.as_view(), name="users_details"),
-    path("users/<int:pk>/", views.UserView.as_view(), name="users"),
+    path("users/<int:id>/", views.UserView.as_view(), name="users"),
     path("products/<int:id>/", views.ProductView.as_view(), name="product"),
     path("products/", views.ProductView.as_view(), name="product_details"),
     path("category/", views.CategoryViewClass.as_view(), name="Category"),
